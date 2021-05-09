b7dd515b439196eb46ae26588e2d3e3e24ba6100
Ann991 <54097641+NarumonThong@users.noreply.github.com>
2021-05-10 00:32:55 +0700

    test api

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const server = express();
-const session = require('express-session');
-const bodyParser = require('body-parser');
+// const session = require('express-session');
+// const bodyParser = require('body-parser');
 const PORT = 3000;
 
-const connect =  require("./configs/database");
+// const connect =  require("./configs/database");
 
 
 // ตั้งค่า Session สำหรับระบบ
-server.use(session({
-    secret: 'barter kub',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {  }
-}));
+// server.use(session({
+//     secret: 'barter kub',
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: {  }
+// }));
 
 // ตั้งค่าการ Parse ตัวแปร เมื่อ Client ส่งข้อมูลเข้ามา
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({ extended: false }));
+// server.use(bodyParser.json());
+// server.use(express.bodyParser());
 
 // สร้าง  Custom function
-server.use(require('./configs/middleware'));
+// server.use(require('./configs/middleware'));
 
 // เรียกใช้งาน router
-server.use('/api', require('./router'));
+// server.use('/api', require('./router'));
 
-server.get('*', (req, res) => {
+server.get('/', (req, res) => {
     res.end(`<h1>Backend Server is Register Token</h1>`)
 });
 
